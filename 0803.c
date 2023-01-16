b84f5ba83ed7a1e5f241fe801747dee6aa7683ed
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2023-01-16 14:13:55 +0100

    Update 0803.c

--- 0803.c
+++ 0803.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-  char vornamen[5][10] = {"Max", "Test", "Olaf", "Günter", "David"};
-  char nachnamen[5][10] = {"Muster", "Name", "Blatt", "Haus", "Braun"};
-  int i;
+  
+    char* vorname[5];
+    vorname[0] = "A";
+    vorname[1] = "B";
+    vorname[2] = "CD";
+    vorname[3] = "E";
+    vorname[4] = "FGH";
+
+    char* nachname[5];
+    nachname[0] = "Eins";
+    nachname[1] = "Zwei";
+    nachname[2] = "Drei";
+    nachname[3] = "Vier";
+    nachname[4] = "Fuenf";
+
+  
+  int i, j;
 
   for (i = 0; i < 5; i++) {
-    printf("%-10s %10s\n", vornamen[i], nachnamen[i]);
+    printf("%s ", vorname[i]);
+    for (j = 0; j < 70 - strlen(vorname[i]) - strlen(nachname[i]); j++) {
+      printf("_");
+    }
+    printf(" %s\n", nachname[i]);
   }
 
   return 0;
