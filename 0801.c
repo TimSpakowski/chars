82eb33462e173d6898062cb755a192e0ba82aca0
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2023-01-16 15:00:17 +0100

    Create 0801.c

---
+++ 0801.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  char productNames[3][20] = {"Äpfel", "Brot", "Nudeln"};
+  int sizes[3] = {500, 1000, 250};
+  float prices[3] = {1.5, 2.0, 1.0};
+  int i;
+
+  for (i = 0; i < 3; i++) {
+    printf("Produktname: %s\n", productNames[i]);
+    printf("Packungsgröße: %d g\n", sizes[i]);
+    printf("Kilopreis netto: %.2f €\n", (prices[i] / sizes[i]) * 1000);
+    printf("Kilopreis brutto: %.2f €\n", ((prices[i] / sizes[i]) * 1000) * 1.07);
+  }
+
+  return 0;
+}
